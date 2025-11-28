a942e2f2ccfdbfa0bb739203e99c85ea9c08e391
Sirjan Kaur Pahuja <sirjan.244052@sggscc.ac.in>
2025-11-28 09:05:41 +0530

    Add iterative insertion sort implementation
    Implement iterative insertion sort algorithm.

---
+++ iterative insertion sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void insertionSortIterative(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        // Shift elements greater than key
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insertionSortIterative(arr, n);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
